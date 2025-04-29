bc2b0cbdae53a11545b50e62b00d7995f3b402fc
Austin M <austin@Austin>
2025-04-28 21:58:01 -0400

    cleaned up setup.sh and better documentation in the script

--- setup.sh
+++ setup.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# This script sets up Vim by:
+# 1. Copying a custom .vimrc configuration file to the user's home directory
+# 2. Installing the Vundle plugin manager (if not already installed)
+# 3. Installing Vim plugins specified in the .vimrc file
+
+
 # Define paths
 VIMRC_SOURCE="$(pwd)/vim-config/.vimrc"
 VIMRC_DEST="$HOME/.vimrc"
 VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim"
 
-# Copy .vimrc to home directory
+# Copy .vimrc to home the user's home directory
 echo "Copying .vimrc to $HOME"
 cp "$VIMRC_SOURCE" "$VIMRC_DEST"
 
-# Install Vundle
+# Install Vundle if it is not already installed
 if [ ! -d "$VUNDLE_DIR" ]; then
     echo "Installing Vundle"
     git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR"
 fi
 
-# Install Vim plugin
+# Install all Vim plugins
 echo "Installing Vim plugin"
 vim +PluginInstall +qall
 
